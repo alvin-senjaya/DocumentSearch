615f9ecdcdda597c8f6f91ee780f7efb754f92d4
alvin-senjaya <alvin_senjaya@yahoo.co.id>
2017-05-16 15:07:34 +1000

    Initialise environment

---
+++ DocumentSearch/Main/Index.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentSearch.Main
+{
+    class Index
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                printErrorMessageWithUsage("Please specify the necessery files.");
+                Environment.Exit(0);
+            }
+            
+            // Preventing the command prompt to close automatically after running the program.
+            Console.Write("Press any key to exit. ");
+            Console.Read();
+        }
+
+        private static void printErrorMessageWithUsage(string error)
+        {
+            Console.Error.WriteLine(error);
+            Console.Error.WriteLine("USAGE: index [-p] <sourcefile> [-s <stoplist>]");
+        }
+    }
+}
